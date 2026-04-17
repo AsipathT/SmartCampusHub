9e921236c4baf2ed95a747391e780dd4a8e0573c
Chanodya Kavindi <chamothebi@gmail.com>
2026-04-17 14:51:37 +0530

    include actor metadata in assign ticket request

--- backend/src/main/java/com/smartcampus/dto/AssignTicketRequest.java
+++ backend/src/main/java/com/smartcampus/dto/AssignTicketRequest.java
@@ -1,10 +1,18 @@
 package com.smartcampus.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AssignTicketRequest {
     @NotNull
     private Long assignedStaffId;
+
+    @NotNull
+    private Long actorUserId;
+
+    @NotNull
+    @Size(min = 2, max = 40)
+    private String actorRole;
 }
